cd41a7e040bf90a2e9c8c5aa90e51e1e4cd297d7
Ignacio Toledo Roll <rolltoledoignacio@gmail.com>
2025-10-19 23:53:31 -0300

    chore: remove console logs from MongoDB connection function

--- src/config/db.js
+++ src/config/db.js
@@ -12,10 +12,8 @@ async function connectDB(uri = process.env.MONGODB_URI || DEFAULT_URI) {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
-    console.log(`MongoDB connected: ${uri}`);
     return mongoose.connection;
   } catch (err) {
-    console.error('MongoDB connection error:', err.message || err);
     throw err;
   }
 }
